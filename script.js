d145ba84cfdd54efa8b7b1e4a02ea965223ba147
Germanual Gross <germanualgross@Germanuals-MacBook-Air.local>
2018-06-02 03:45:49 -0500

    updating code to all files.

---
+++ script.js
@@ -0,0 +1,13 @@
+// Pretty Girls Love Trap Music JS and jQuery
+// ==========================================
+
+$(document).ready(function() {
+
+// Cards fade out on scroll up.
+// ============================
+
+$(".jumbotron").scroll(function() {
+  $(".card-columns").fadeOut(3000);
+});
+
+});
